3e080ca3bbb3fc1ad2ba960dc588c997a9009d47
py-react <yksandeep08+reactpy@gmail.com>
2024-07-28 18:21:13 +0530

    api example

--- gingerjs/create_app/app/src/app/api/test/index.py
+++ gingerjs/create_app/app/src/app/api/test/index.py
@@ -1,2 +1,11 @@
-async def index(request):
-    return {"api":"test"}
+async def GET(request):
+    return {"user":"1"}
+
+async def POST(request):
+    return {"message":"done"}
+
+async def PUT(request):
+    return {"message":"done"}
+
+async def DELETE(request):
+    return {"message":"done"}
